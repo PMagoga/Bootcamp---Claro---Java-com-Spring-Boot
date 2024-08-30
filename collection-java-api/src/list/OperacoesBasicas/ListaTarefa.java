7c58c6e8a6f1520ce141d18e2283d3ebcb78b5df
PMagoga <pmagoga@gmail.com>
2024-08-30 20:58:31 -0300

    adicionando README

--- collection-java-api/src/list/OperacoesBasicas/ListaTarefa.java
+++ collection-java-api/src/list/OperacoesBasicas/ListaTarefa.java
@@ -32,4 +32,15 @@ public class ListaTarefa {
     public void obterDescricaoTarefa(){
         System.out.println(tarefaList);
     }
+
+    public static void main(String[] args) {
+        ListaTarefa listarefa = new ListaTarefa();
+        // verificar se a lista está vazia
+        System.out.println("O número total de elementos na lista é: " + listarefa.obterNumeroTotalTarefas());
+
+        // adicionando uma tarefa
+        listarefa.adicionarTarefa("Tarefa 1");
+        // verificando o tamanho da lista
+        System.out.println("O número total de elementos na lista é: " + listarefa.obterNumeroTotalTarefas());
+    }
 }
