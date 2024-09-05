8bca08dbbb7f0e5456c9d355d7eee4bf0095cfd7
PMagoga <pmagoga@gmail.com>
2024-09-05 20:17:04 -0300

    Desafio de código verificando velocidade média

---
+++ src/velocidadeMedia/VerificandoVelocidadeMedia.java
@@ -0,0 +1,35 @@
+package velocidadeMedia;
+
+import java.util.Scanner;
+
+public class VerificandoVelocidadeMedia {
+    // Função para calcular a velocidade média de conexão de internet
+    public static double calcularVelocidadeMedia(String[] velocidades) {
+        int total = velocidades.length;
+        int soma = 0;
+        // TODO: Some todas as velocidades registradas e calcule a média
+        for(String velocidade : velocidades){
+            int velocidadeInteiro = Integer.parseInt(velocidade.trim());
+            soma += velocidadeInteiro;
+        }
+        return soma / total;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando ao usuário a lista de velocidades de conexão registradas a cada hora
+        String input = scanner.nextLine();
+
+        // Convertendo a entrada em uma lista de strings
+        String[] velocidades = input.split(",");
+
+        // Calculando a velocidade média de conexão
+        double velocidadeMedia = calcularVelocidadeMedia(velocidades);
+
+        // Exibindo a velocidade média de conexão
+        System.out.println((int)velocidadeMedia + " Mbps");
+
+        scanner.close();
+    }
+}
