7c724212f6d6a7014de390d7c675be43def95763
PMagoga <pmagoga@gmail.com>
2024-09-21 17:05:55 -0300

    monitor temperatura CPU

---
+++ src/explorandoapi/temperaturacpu/CpuTemperatureMonitor.java
@@ -0,0 +1,26 @@
+package explorandoapi.temperaturacpu;
+
+import java.util.Scanner;
+
+public class CpuTemperatureMonitor {
+    public static String findHighestTemperature(String input) {
+        String[] readings = input.split(",");
+        int maxTemperature = Integer.MIN_VALUE;
+
+        for (String reading : readings) {
+            int temperature = Integer.parseInt(reading.trim());
+            // TODO: Comparar a temperatura convertida com maxTemperature e atualizar maxTemperature se a leitura atual for maior.
+
+        }
+
+        // TODO: Retorne a maior temperatura:
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String output = findHighestTemperature(input);
+        System.out.println(output);
+    }
+}
